4564538683fbfcb9bb13387c0835e5e756424e0d
AhmedZake33 <ahmed.zake333@gmail.com>
2023-08-18 17:56:23 +0300

    fix

--- app/Http/Requests/loginRequest.php
+++ app/Http/Requests/loginRequest.php
@@ -33,7 +33,7 @@ class loginRequest extends FormRequest
     {
         return [
             'mobile' => [
-                'required','regex:/^(\+?\d{1,3}[- ]?)?\d{13}$/',
+                'required','regex:/^(\+?\d{1,3}[- ]?)?\d{10}$/',
                 "exists:users,mobile"
                 
             ]
