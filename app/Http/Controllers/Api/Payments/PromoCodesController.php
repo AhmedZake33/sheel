a3db44b7a0cfcfd2a466b8108739a5d8324f424e
AhmedZake33 <ahmed.zake333@gmail.com>
2023-09-09 23:53:26 +0300

    fix

--- app/Http/Controllers/Api/Payments/PromoCodesController.php
+++ app/Http/Controllers/Api/Payments/PromoCodesController.php
@@ -12,12 +12,15 @@ class PromoCodesController extends Controller
 {
     public function check(Request $request)
     {
-        $promocode = PromoCode::where('code',$request->code)->firstOrFail(); 
+        $promocode = PromoCode::where('code',$request->code)->first(); 
+        if(!$promocode){
+            $message = $this->lang == 'en' ? 'Promo Code Valid':'رمز ترويجي صحيح';
+            return success($promocode->data(System::DATA_BRIEF) , System::HTTP_OK , $message );
+        }
         $counts =  $promocode->payments->count();
         $now = Carbon::now()->toDateString();
         if($promocode->counts > $counts && $now < $promocode->expiration_date){
-            $message = $this->lang == 'en' ? 'Promo Code Valid':'رمز ترويجي صحيح';
-            return success($promocode->data(System::DATA_BRIEF) , System::HTTP_OK , $message );
+            
         }
         $message = $this->lang == 'en' ? 'Promo Code Not Valid or Expired':' رمز ترويجي غير صحيح او منتهي ';
         return success([],System::HHTP_Unprocessable_Content ,$message);
