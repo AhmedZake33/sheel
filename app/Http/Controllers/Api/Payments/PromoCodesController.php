7d6213c9ac2a99e9e2c25a92011bcc677746fd4f
AhmedZake33 <ahmed.zake333@gmail.com>
2023-09-09 23:56:09 +0300

    fix

--- app/Http/Controllers/Api/Payments/PromoCodesController.php
+++ app/Http/Controllers/Api/Payments/PromoCodesController.php
@@ -20,9 +20,11 @@ class PromoCodesController extends Controller
         $counts =  $promocode->payments->count();
         $now = Carbon::now()->toDateString();
         if($promocode->counts > $counts && $now < $promocode->expiration_date){
-            
+            $message = $this->lang == 'en' ? 'Promo Code Valid':'رمز ترويجي صحيح';
+            return success($promocode->data(System::DATA_BRIEF) , System::HTTP_OK , $message );
         }
-        
+        $message = $this->lang == 'en' ? 'Promo Code Not Valid or Expired':' رمز ترويجي غير صحيح او منتهي ';
+        return success([],System::HHTP_Unprocessable_Content ,$message);
         
     }
 }
