a8ceb753de8501ce7f136ea863f4c4ce848ed3da
AhmedZake33 <ahmed.zake333@gmail.com>
2024-03-18 22:55:15 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get("test",function(){
+    return "test here";
+});
+
 Route::get('/login', function () {
     return view('login');
 })->name('login');
