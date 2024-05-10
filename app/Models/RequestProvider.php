9ecf3d148a61d35be36116ba12d8d30891af333d
AhmedZake33 <ahmed.zake333@gmail.com>
2024-05-10 16:54:40 +0300

    fix

--- app/Models/RequestProvider.php
+++ app/Models/RequestProvider.php
@@ -54,7 +54,7 @@ class RequestProvider extends Model
     public function data()
     {
         $data = (Object)[];
-        $data->user = $this->provider ? $this->provider->user : null;
+        $data->data = $this->provider ? $this->provider->user : null;
         $data->status = $this->decodeStatus($this->status);
         return $data;
     }
