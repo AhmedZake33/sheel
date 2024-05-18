33d5d6c06d15053c932b10fa217c1002677786f9
AhmedZake33 <ahmed.zake333@gmail.com>
2024-05-18 17:54:27 +0300

    fix

--- app/Services/RequestService.php
+++ app/Services/RequestService.php
@@ -50,9 +50,9 @@ class RequestService extends Base
 
         $requestModel->startFindProvider();
 
-        if(domain() != "http://127.0.0.1:8000" && $user->id != 6){
-            $requestModel->autoAssignProvider(5);
-        }
+        // if(domain() != "http://127.0.0.1:8000" && $user->id != 6){
+        //     $requestModel->autoAssignProvider(5);
+        // }
         
         
         // if($request->card_id){
