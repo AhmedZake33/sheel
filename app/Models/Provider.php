93d7240209f7b5442c00fa7c208c0de9c6ddc426
AhmedZake33 <ahmed.zake333@gmail.com>
2024-05-18 18:11:32 +0300

    fix

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\requestProvider;
 use Illuminate\Database\Eloquent\Model;
 
 class Provider extends Model
