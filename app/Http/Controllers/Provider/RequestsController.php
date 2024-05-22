bbe3c7ce80abd7d4448120a4da0ab46836000754
AhmedZake33 <ahmed.zake333@gmail.com>
2024-05-23 01:05:12 +0300

    fix

--- app/Http/Controllers/Provider/RequestsController.php
+++ app/Http/Controllers/Provider/RequestsController.php
@@ -179,6 +179,8 @@ class RequestsController extends Controller
                 // fire event 
                 event(new \App\Events\CurrentRequests($requestData->user_id , $requestData));
                 return success([],System::HTTP_OK , "SUCCESS");
+            }else{
+                return success([],System::HHTP_Unprocessable_Content , "IMAGE UPLOADED BEFORE");
             }
         }
         return success([],System::HHTP_Unprocessable_Content , "CANNOT UPLOAD IMAGE");
@@ -206,6 +208,8 @@ class RequestsController extends Controller
                 event(new \App\Events\CurrentRequests($requestData->user_id , $requestData));
                 
                 return success([],System::HTTP_OK , "SUCCESS");
+            }else{
+                return success([],System::HHTP_Unprocessable_Content , "IMAGE UPLOADED BEFORE");
             }
         }
         return success([],System::HHTP_Unprocessable_Content , "CANNOT UPLOAD IMAGE");
