130a92b969e43f762509eb6d969d9c0c1e6745a8
AhmedZake33 <ahmed.zake333@gmail.com>
2024-05-24 16:35:00 +0300

    fix

--- app/Models/payment/Payment.php
+++ app/Models/payment/Payment.php
@@ -25,7 +25,7 @@ class Payment extends Model
 
     public function promoCode()
     {
-        return $this->belongsTo(PromoCode::class,"promo_code_id","id");
+        return $this->belongsTo(PromoCode::class);
     }
 
     public static function createAndUpdate($data)
