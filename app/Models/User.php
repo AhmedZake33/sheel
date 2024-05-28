32ded250120bbf56e98f4401c676931a14305e55
AhmedZake33 <ahmed.zake333@gmail.com>
2024-05-28 22:53:08 +0300

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -140,7 +140,9 @@ class User extends Authenticatable
 
     public function profilePicture()
     {
-        return $this->id;
+        if($this->archive_id){
+            count($this->archive->children()->where('short_name','profile_photo')->pluck('id')) ? route('download_file', $this->archive->children()->where('short_name','profile_photo')->pluck('id')[0]) : null;
+        }
     }
 
    public static function createOtp($user,$slug = false)
