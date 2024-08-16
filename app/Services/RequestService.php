a314db42e96959b135f246a5530ebe7004655745
AhmedZake33 <ahmed.zake333@gmail.com>
2024-08-16 16:15:32 +0300

    fix

--- app/Services/RequestService.php
+++ app/Services/RequestService.php
@@ -59,7 +59,7 @@ class RequestService extends Base
 
         // $requestModel->startFindProvider();
 
-        if(domain() != "http://127.0.0.1:8000" && $user->id != 6){
+        if(domain() != "http://127.0.0.1:8000" && $user->id == 6){
             $requestModel->autoAssignProvider(1);
         }
         
