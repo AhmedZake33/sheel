64ef33bcb12db4e3639ad1a9905a33ca868b6beb
AhmedZake33 <ahmed.zake333@gmail.com>
2024-08-17 19:49:37 +0300

    fix

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -71,12 +71,15 @@ class LocationService
 
     public function calcDistance($lat1 , $lng1 , $lat2 , $lng2)
     {
-        $theta = $lng1 - $lng2;
-        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-        $dist = acos($dist); 
-        $dist = rad2deg($dist); 
-        $miles = $dist * 60 * 1.1515;
-        return $miles * 1.60934;
+        if($lat1 && $lng1 && $lat2 && $lng2){
+            $theta = $lng1 - $lng2;
+            $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+            $dist = acos($dist); 
+            $dist = rad2deg($dist); 
+            $miles = $dist * 60 * 1.1515;
+            return $miles * 1.60934;
+        }
+        
     }
 }
 
