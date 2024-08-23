746255fadd7cd6299c433a6c8de44365d32a1d63
AhmedZake33 <ahmed.zake333@gmail.com>
2024-08-23 17:07:00 +0300

    fix

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -346,11 +346,11 @@ class Request extends Model
                 $existProvider = RequestProvider::where('request_id',$this->id)->where('status',RequestProvider::STATUS_PENDING)->first();
                 if(!$existProvider){
                     $providerRequestService->assignProvider($nearestLocation->user_id , $this->id);
-                    event(new \App\Events\CurrentRequests($nearestLocation->user_id , $this));
+                    // event(new \App\Events\CurrentRequests($nearestLocation->user_id , $this));
                 }
                 // notification to provider
-                // $title = ['ar' => 'لقد تم اضافتك الي طلب' , 'en' => 'you have assigned to request'];
-                // Notification::createNotification($nearestLocation->user_id , $this->id , $title);
+                $title = ['ar' => 'لقد تم اضافتك الي طلب' , 'en' => 'you have assigned to request'];
+                Notification::createNotification($nearestLocation->user_id , $this->id , $title);
             }
             
         }
